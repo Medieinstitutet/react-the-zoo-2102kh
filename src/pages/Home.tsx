0dd250b279375fa7c9a2e6287333c285e43ccf96
2102kh <144224944+2102kh@users.noreply.github.com>
2024-08-22 16:36:48 +0200

    :sparkles:Fixed some bugs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ export const Home=()=>{
     <div className="home-background">
     <div className="video-player">
       <video  className="video" autoPlay loop muted>
-      <source src="/src/media/118059-713859239_tiny.mp4" type="video/mp4"/>
+      <source src="/public/118059-713859239_tiny.mp4" type="video/mp4"/>
       Your browser does not support the video tag.
       </video>
     </div>
